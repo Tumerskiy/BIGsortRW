8dabb588f49f28cb0a3809006ba167d5ab8ba4a9
MacBook <macbook@MBP-MacBook.local>
2018-02-08 23:27:54 -0500

    modified Entries Added Stream sort:

--- src/Entries.java
+++ src/Entries.java
@@ -1,10 +1,7 @@
-import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.NumberFormat;
-import java.util.Comparator;
-import java.io.BufferedReader;
+import java.util.Random;
 import java.io.BufferedWriter;
 public class Entries {
 	public static void main(String[] args) {
@@ -12,19 +9,23 @@ public class Entries {
 
 		NumberFormat format = NumberFormat.getInstance();
 
-		StringBuilder sb = new StringBuilder();
+		
 		long maxMemory = runtime.maxMemory();
 		long allocatedMemory = runtime.totalMemory();
 		long freeMemory = runtime.freeMemory();
-		String haString = "12345678John   Smiths 111222999999999 1455     Maisonneuve West, Montreal, QC, H3G 1M8";
-		String heString = "88888888Roselyn Shiri 222444888888888 1515 Saint-Catherine West, Montreal, QC, H3G 1M7";
-		sb.append("free memory: " + format.format(freeMemory / 1024) + "\n");
-		sb.append("allocated memory: " + format.format(allocatedMemory / 1024) + "\n");
-		sb.append("max memory: " + format.format(maxMemory / 1024) + "\n");
-		sb.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "\n");
-		sb.append("read # entries: "+ format.format(maxMemory /200) +"\n");
-		sb.append("read # blocks: "+ format.format(maxMemory /200/40) +"\n");
 		
+		Random random  = new Random();
+		
+		
+		String haString = "John   Smiths 111222999999999 1455             Maisonneuve West, Montreal,       QC, H3G 1M8";
+//		sb.append("free memory: " + format.format(freeMemory / 1024) + "\n");
+//		sb.append("allocated memory: " + format.format(allocatedMemory / 1024) + "\n");
+//		sb.append("max memory: " + format.format(maxMemory / 1024) + "\n");
+//		sb.append("total free memory: " + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024) + "\n");
+//		sb.append("read # entries: "+ format.format(maxMemory /200) +"\n");
+//		sb.append("read # blocks: "+ format.format(maxMemory /200/40) +"\n");
+		
+		System.out.println(haString.length());
 		BufferedWriter bw1 = null;
 		try {
 			bw1 = new BufferedWriter(new FileWriter("input1.txt"));
@@ -32,19 +33,32 @@ public class Entries {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		for (int j=0;j<500000;j++) {
+		for (long j=0;j<100000;j++) {
 			try {
-				bw1.write(haString);
+				StringBuilder sb = new StringBuilder();
+				sb.append(random.nextInt(10));
+				sb.append(random.nextInt(10));
+				sb.append(random.nextInt(10));
+				sb.append(random.nextInt(10));
+				sb.append(random.nextInt(10));
+				sb.append(random.nextInt(10));
+				sb.append(random.nextInt(10));
+				sb.append(random.nextInt(10));
+				sb.append(haString);
 				bw1.newLine();
-				bw1.write(heString);
-				bw1.newLine();
-			} catch (IOException e) {
+				bw1.write(sb.toString());
+				
+			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			
 		}
+		try {
+			bw1.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
 		
-		System.out.println(sb);
 	}
 }
--- src/mainPart.java
+++ src/mainPart.java
@@ -1,19 +1,48 @@
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.nio.Buffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.awt.List;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 
 public class mainPart {
+	
+	
+	public static void writeToFile (String s, FileWriter fw) {
+		try {
+			fw.write(String.format("%s%n", s));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
 	private static File inFile1 = new File("input1.txt");
 	private static File inFile2 = new File("");
 	private static File outFile1 = new File("output1.txt");
 	private static File outFile2 = new File("");
+	
+	
 	private static ArrayList<String> buffer;
+	
+	
 	public static void main(String[] args) {
+		Comparator<String> sAscComp = (s1, s2) -> s1.substring(0, 8).compareTo(s2.substring(0, 8));
 		long startTime = System.currentTimeMillis();
 		BufferedReader br1 = null;
 		BufferedReader br2 = null;
@@ -23,6 +52,7 @@ public class mainPart {
 		try {
 			br1 = new BufferedReader(new FileReader(inFile1));
 			bw1 = new BufferedWriter(new FileWriter(outFile1));
+			final FileWriter fw = new FileWriter(outFile1);
 			String content = null;
 			int i=0;
 			int number = 0;
@@ -37,7 +67,20 @@ public class mainPart {
 						number++;
 					}
 				}
-				Collections.sort(buffer, new AscComparator());
+				//Collections.sort(buffer, new AscComparator());
+				
+				
+				buffer =(ArrayList<String>) buffer
+					.stream()
+					.sorted(sAscComp)
+					.collect(Collectors.toList());
+				 
+//				buffer
+//					.stream()
+//					.sorted(sAscComp)
+//					.forEach(s -> writeToFile(s,fw));
+				
+					
 				
 				for (i = 0;i<buffer.size();i++) {
 					bw1.newLine();
@@ -82,6 +125,22 @@ public class mainPart {
 		}
 	}
 }
+
+class threadSorter implements Callable<ArrayList<String>>{
+	ArrayList<String> sorted;
+	public threadSorter(ArrayList<String> ar) {
+		sorted = ar;
+	}
+
+	@Override
+	public ArrayList<String> call() throws Exception {
+		Collections.sort(sorted, new AscComparator());
+		return sorted;
+	}
+	
+
+}
+
 
 class AscComparator implements Comparator <String>{
 
