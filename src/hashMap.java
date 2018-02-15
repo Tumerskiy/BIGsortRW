b44ad47cf0249ccde8a3c234cf1b50e503c83a5b
Tumer Horloev <tumer.horloev@ericsson.com>
2018-02-14 21:30:14 -0500

    TREEMAP

--- src/hashMap.java
+++ src/hashMap.java
@@ -7,6 +7,7 @@ import java.io.BufferedWriter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class hashMap {
@@ -20,7 +21,7 @@ public class hashMap {
 
     private static ArrayList<String> buffer;
 
-    private static HashMap<String, Integer> map = new HashMap<String, Integer>();
+    private static TreeMap<String, Integer> map = new TreeMap<String, Integer>();
 
 
 
@@ -42,245 +43,255 @@ public class hashMap {
 
             //here is the map landing from 2 files
             while(((content=br1.readLine())!=null)){
-                if (map.size()>20000){
-                    for (String p: map.keySet()){
-                        bw1.write(p+":"+map.get(p)+"\n");
-                    }
-                    map.clear();
-                }
-                for (i = 0;i<2000;i++) {
+                 
+                for (i = 0;i<10000;i++) {
                     content=br1.readLine();
                     if(content!=null) {
                         buffer.add(content);
                     }
                 }
                 for (i = 0;i<buffer.size();i++) {
-                    map.merge(buffer.get(i).substring(0,8), 1, Integer::sum);
+                	if (map.get(buffer.get(i))==null) {
+                		map.put(buffer.get(i), 1);
+                	} else {
+                		map.put(buffer.get(i), map.get(buffer.get(i))+1);
+                	}
 
                 }
                 buffer.clear();
 
-                for (i = 0;i<2000;i++) {
+                for (i = 0;i<10000;i++) {
                     content=br2.readLine();
                     if(content!=null) {
                         buffer.add(content);
                     }
                 }
                 for (i = 0;i<buffer.size();i++) {
-//                    if (buffer.get(i).length()<8){
-//                        System.out.println(buffer.get(i));
-//                    }
-                    map.merge(buffer.get(i).substring(0,8), -1, Integer::sum);
+                	if (map.get(buffer.get(i))==null) {
+                		map.put(buffer.get(i), -1);
+                	} else {
+                		map.put(buffer.get(i), map.get(buffer.get(i))-1);
+                	}
                 }
                 buffer.clear();
+                for (String p: map.keySet()){
+                    bw1.write(p+" "+map.get(p)+"\n");
+                }
+                map.clear();
             }
             //some cleanup
             br1.close();
             br2.close();
             bw1.close();
-            br1 = new BufferedReader(new FileReader(premap));
-            bw1 = new BufferedWriter(new FileWriter(premap1));
-            content=null;
-            buffer.clear();
-            map.clear();
-            Comparator<String> sAscComp = (s1, s2) -> s1.substring(0, 8).compareTo(s2.substring(0, 8));
-//this is sort of premap, and as a result you have somewhat presorted map, but with unmerged values
-            int numberOfSublist = 0;
-            int number = 0;
-            while(((content=br1.readLine())!=null)){
-                numberOfSublist ++;
-                for (i = 0;i<40000;i++) {
-                    content=br1.readLine();
-                    if(content!=null) {
-                        buffer.add(content);
-                        number++;
-                        
-                    }
-                }
-                buffer =(ArrayList<String>) buffer
-                        .stream()
-                        .sorted(sAscComp)
-                        .collect(Collectors.toList());
+            
+            
+//           //merger 
+//            br1 = new BufferedReader(new FileReader(premap));
+//            bw1 = new BufferedWriter(new FileWriter(premap1));
+//            content=null;
+//            buffer.clear();
+//            map.clear();
+//            Comparator<String> sAscComp = (s1, s2) -> s1.substring(0, 100).compareTo(s2.substring(0, 100));
+////this is sort of premap, and as a result you have somewhat presorted map, but with unmerged values
+//            int numberOfSublist = 0;
+//            int number = 0;
+//            while(((content=br1.readLine())!=null)){
+//                numberOfSublist ++;
+//                for (i = 0;i<15000;i++) {
+//                    content=br1.readLine();
+//                    if(content!=null) {
+//                        buffer.add(content);
+//                        number++;
+//                        
+//                    }
+//                }
+//                buffer =(ArrayList<String>) buffer
+//                        .stream()
+//                        .sorted(sAscComp)
+//                        .collect(Collectors.toList());
+//
+//                for (i = 0;i<buffer.size();i++) {                   
+//                    bw1.write(buffer.get(i));
+//                    bw1.newLine();
+//                    
+//
+//                }
+//                buffer.clear();
+//            }
+//            
+//            
+//
+//            //cleanup again
+//            br1.close();
+//            bw1.close();
+//            map.clear();
+//            content = null;
+            
+//            //merge
+//            
+       //     System.out.println(numberOfSublist);
+//            int[] indexesN = new int[numberOfSublist];
+//            
+//            String[] compareBuffer = new String[numberOfSublist];
+//            ArrayList<BufferedReader> indexes = new ArrayList<>();
+//            for (int j=0;j<numberOfSublist;j++) {
+//                    br1 = new BufferedReader(new FileReader(premap1));
+//                    for (int skip=0; skip<40000*j;skip++) {
+//                        br1.readLine();
+//                    }
+//                    indexes.add(br1);
+//                    compareBuffer[j] = (br1.readLine());
+//                    indexesN[j]= 1;
+//            }
+//            
+//            bw1 = new BufferedWriter(new FileWriter(damap));
+//            
+//            String min = compareBuffer[0].substring(0, 8);
+//            String in = null;
+//            int minIndex = 0;
+//            int p = 0;
+//            int c = 0;
+//            while (p<number) {
+//                c = 0;
+//                min = "100000000";
+//                    
+//                    minIndex = 0;
+//                for (int j = 0;j<numberOfSublist;j++) {// find min
+//                        
+////                      System.out.println(j+") "+compareBuffer[j]);
+//                    
+//                        if (compareBuffer[j]!=null) {
+//                                in = compareBuffer[j].substring(0, 8);
+//                                if (Integer.parseInt(min)> Integer.parseInt(in)) {
+//                                    min = in;
+//                                    minIndex = j;
+//                                }
+//                            }
+//                        
+//                }
+//                if (compareBuffer[minIndex]==null) break;
+//                c = Integer.parseInt(compareBuffer[minIndex].substring(9));
+//                
+//                if (indexesN[minIndex]<40000) {
+//                        compareBuffer[minIndex] = indexes.get(minIndex).readLine();
+//                        indexesN[minIndex]++;
+//                        p++;
+//                    }
+//                else {
+//                    compareBuffer[minIndex] = null;
+//                }
+//                
+//                
+//                
+////              System.out.println("min: "+minIndex+" "+min);
+//                
+//                
+//                
+////              System.out.println("replaced:");
+//                
+////              for (int j = 0;j<numberOfSublist;j++) {
+////                      System.out.println(" "+j+") "+compareBuffer[j]);                    
+////              }
+//                
+//                
+//                for (int j = 0;j<numberOfSublist;j++) {
+//                    if (compareBuffer[j]!=null && min != null)
+//                        if (min.equals(compareBuffer[j].substring(0, 8))){
+//                            //System.out.println(min+"<->"+compareBuffer[j]+" "+j);
+//                            c += Integer.parseInt(compareBuffer[j].substring(9));
+//                            //System.out.println(min+":   "+counter);
+//                            if (indexesN[j]<40000) {
+//                                compareBuffer[j] = indexes.get(j).readLine();
+//                                indexesN[j]++;
+//                                p++;    
+//                            }
+//                            else {
+//                                compareBuffer[j] = null;
+//                            }
+//                            
+//                            
+//                            j--;
+//                        }
+//                }
+//                //System.out.println(p+")"+min+": "+c);             
+//                //min = compareBuffer[1].substring(0, 8);
+//                if (c<0) c=0;
+//                bw1.write(min+":"+c);
+//                bw1.newLine();
+//                
+//                        
+//            }
+//            
+//            bw1.close();
+//            for (int j = 0;j<numberOfSublist;j++) {
+//                    indexes.get(j).close();
+//            }
+//            
+//            indexes.clear();
+//            min = null;
+//            in = null;
+//            indexesN = null;
+//            compareBuffer = null;
+//            minIndex = 0;
+//            number = 0;
+//            numberOfSublist = 0;
+//            
+//            
+//            //result generator
+//            br1.close();
+//            br2.close();
+            
 
-                for (i = 0;i<buffer.size();i++) {                   
-                    bw1.write(buffer.get(i));
-                    bw1.newLine();
-                    
-
-                }
-                buffer.clear();
-            }
+            // last phase
             
             
-
-            //cleanup again
-            br1.close();
-            bw1.close();
-            map.clear();
-            content = null;
-            
-            //merge
-            
-            System.out.println(numberOfSublist);
-            int[] indexesN = new int[numberOfSublist];
-            
-            String[] compareBuffer = new String[numberOfSublist];
-            ArrayList<BufferedReader> indexes = new ArrayList<>();
-            for (int j=0;j<numberOfSublist;j++) {
-                    br1 = new BufferedReader(new FileReader(premap1));
-                    for (int skip=0; skip<40000*j;skip++) {
-                        br1.readLine();
-                    }
-                    indexes.add(br1);
-                    compareBuffer[j] = (br1.readLine());
-                    indexesN[j]= 1;
-            }
-            
-            bw1 = new BufferedWriter(new FileWriter(damap));
-            
-            String min = compareBuffer[0].substring(0, 8);
-            String in = null;
-            int minIndex = 0;
-            int p = 0;
-            int c = 0;
-            while (p<number) {
-                c = 0;
-                min = "100000000";
-                    
-                    minIndex = 0;
-                for (int j = 0;j<numberOfSublist;j++) {// find min
-                        
-//                      System.out.println(j+") "+compareBuffer[j]);
-                    
-                        if (compareBuffer[j]!=null) {
-                                in = compareBuffer[j].substring(0, 8);
-                                if (Integer.parseInt(min)> Integer.parseInt(in)) {
-                                    min = in;
-                                    minIndex = j;
-                                }
-                            }
-                        
-                }
-                if (compareBuffer[minIndex]==null) break;
-                c = Integer.parseInt(compareBuffer[minIndex].substring(9));
-                
-                if (indexesN[minIndex]<40000) {
-                        compareBuffer[minIndex] = indexes.get(minIndex).readLine();
-                        indexesN[minIndex]++;
-                        p++;
-                    }
-                else {
-                    compareBuffer[minIndex] = null;
-                }
-                
-                
-                
-//              System.out.println("min: "+minIndex+" "+min);
-                
-                
-                
-//              System.out.println("replaced:");
-                
-//              for (int j = 0;j<numberOfSublist;j++) {
-//                      System.out.println(" "+j+") "+compareBuffer[j]);                    
-//              }
-                
-                
-                for (int j = 0;j<numberOfSublist;j++) {
-                    if (compareBuffer[j]!=null && min != null)
-                        if (min.equals(compareBuffer[j].substring(0, 8))){
-                            //System.out.println(min+"<->"+compareBuffer[j]+" "+j);
-                            c += Integer.parseInt(compareBuffer[j].substring(9));
-                            //System.out.println(min+":   "+counter);
-                            if (indexesN[j]<40000) {
-                                compareBuffer[j] = indexes.get(j).readLine();
-                                indexesN[j]++;
-                                p++;    
-                            }
-                            else {
-                                compareBuffer[j] = null;
-                            }
-                            
-                            
-                            j--;
-                        }
-                }
-                //System.out.println(p+")"+min+": "+c);             
-                //min = compareBuffer[1].substring(0, 8);
-                if (c<0) c=0;
-                bw1.write(min+":"+c);
-                bw1.newLine();
-                
-                        
-            }
-            
-            bw1.close();
-            for (int j = 0;j<numberOfSublist;j++) {
-                    indexes.get(j).close();
-            }
-            
-            indexes.clear();
-            min = null;
-            in = null;
-            indexesN = null;
-            compareBuffer = null;
-            minIndex = 0;
-            number = 0;
-            numberOfSublist = 0;
-            
-            
-            //result generator
-            br1.close();
-            br2.close();
-            
-            
-            
-            br1 = new BufferedReader(new FileReader(damap));
-            bw1 = new BufferedWriter(new FileWriter(result));
-           
-            buffer = null;
-            
-            int mapChunk = 20000;
-            int iChunk = 4000;
-            ArrayList<String>  buff1 = new ArrayList<>();
-            content = null;
-            String fLine = null;
-            HashMap<String, String> sBuff1 = new HashMap<String, String>();
-            
-            while((content=br1.readLine())!=null) {
-                buff1.add(content);
-                for (i=0;i<mapChunk;i++) {
-                    content=br1.readLine();
-                    if(content!=null) {
-                        buff1.add(content);
-                    }                    
-                }
-                //System.out.println("Map chunk is here");
-                br2 = new BufferedReader(new FileReader(inFile1));
-                while((fLine=br2.readLine())!=null){
-                    
-                    for (i=0;i<iChunk;i++) {
-                        fLine = br2.readLine();
-                        if(fLine!=null) {
-                            sBuff1.put(fLine.substring(0,8),fLine);
-                        }
-                    }
-                    //System.out.println("chunk t1 is here");
-                    for(String mapLine : buff1) {
-                        if (sBuff1.get(mapLine.substring(0,8))!=null) {
-                        bw1.write(sBuff1.get(mapLine.substring(0,8))+" "+mapLine.substring(9)+"\n");
-                        }
-
-                    }
-                    sBuff1.clear();
-                    
-                }
-                buff1.clear();
-                
-            }            
-            br1.close();
-            br2.close();
-            bw1.close();
+//            
+//            br1 = new BufferedReader(new FileReader(damap));
+//            bw1 = new BufferedWriter(new FileWriter(result));
+//           
+//            buffer = null;
+//            
+//            int mapChunk = 20000;
+//            int iChunk = 4000;
+//            ArrayList<String>  buff1 = new ArrayList<>();
+//            content = null;
+//            String fLine = null;
+//            HashMap<String, String> sBuff1 = new HashMap<String, String>();
+//            
+//            while((content=br1.readLine())!=null) {
+//                buff1.add(content);
+//                for (i=0;i<mapChunk;i++) {
+//                    content=br1.readLine();
+//                    if(content!=null) {
+//                        buff1.add(content);
+//                    }                    
+//                }
+//                //System.out.println("Map chunk is here");
+//                br2 = new BufferedReader(new FileReader(inFile1));
+//                while((fLine=br2.readLine())!=null){
+//                    
+//                    for (i=0;i<iChunk;i++) {
+//                        fLine = br2.readLine();
+//                        if(fLine!=null) {
+//                            sBuff1.put(fLine.substring(0,8),fLine);
+//                        }
+//                    }
+//                    //System.out.println("chunk t1 is here");
+//                    for(String mapLine : buff1) {
+//                        if (sBuff1.get(mapLine.substring(0,8))!=null) {
+//                        bw1.write(sBuff1.get(mapLine.substring(0,8))+" "+mapLine.substring(9)+"\n");
+//                        }
+//
+//                    }
+//                    sBuff1.clear();
+//                    
+//                }
+//                buff1.clear();
+//                
+//            }            
+//            br1.close();
+//            br2.close();
+//            bw1.close();
             
             
             long stopTime = System.currentTimeMillis();
