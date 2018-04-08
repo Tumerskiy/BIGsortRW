100bf54e3cf05d2b8ead7f1521fe2ca2fb9d02f5
MacBook <macbook@MBP-MacBook.local>
2018-04-08 17:03:32 -0400

    BIGdecimal version extremely slow

--- src/MP1.java
+++ src/MP1.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.io.BufferedReader;
 import java.io.BufferedWriter; 
 import java.util.ArrayList;
@@ -13,50 +15,50 @@ public class MP1 {
 	private static File result = new File("LoopResult.txt");
 	private static File gpaResult = new File("Loopgpa.txt");
 	
-	public static double convertGrade(String grade) {
+	public static String convertGrade(String grade) {
 		switch (grade.trim()) {
 		case "A+":
 //			System.out.println("A+");
-			return 4.3;
+			return "4.3";
 		case "A":
 //			System.out.println("A");
-			return 4.0;
+			return "4.0";
 		case "A-":
 //			System.out.println("A-");
-			return 3.7;
+			return "3.7";
 		case "B+":
 //			System.out.println("B+");
-			return 3.3;
+			return "3.3";
 		case "B":
 //			System.out.println("B");
-			return 3.0;
+			return "3.0";
 		case "B-":
 //			System.out.println("B-");
-			return 2.7;
+			return "2.7";
 		case "C+":
 //			System.out.println("C+");
-			return 2.3;
+			return "2.3";
 		case "C":
 //			System.out.println("C");
-			return 2.0;
+			return "2.0";
 		case "C-":
 //			System.out.println("C-");
-			return 1.7;
+			return "1.7";
 		case "D+":
 //			System.out.println("D+");
-			return 1.3;
+			return "1.3";
 		case "D":
 //			System.out.println("D");
-			return 1.0;
+			return "1.0";
 		case "D-":
 //			System.out.println("D-");
-			return 0.7;
+			return "0.7";
 		case "Fail":
 //			System.out.println("F");
-			return 0;
+			return "0.0";
 
 		default:
-			return -1;
+			return "0.0";
 		}
 	}
 	
@@ -69,13 +71,13 @@ public class MP1 {
         BufferedReader br2 = null;
         BufferedWriter bw1 = null;
         BufferedWriter bw2 = null;
-        int bbs1 = 8400;
+        int bbs1 = 7150;
 //        int bbs2 = 10000;
         String content = null;
         long numberOfIO = 0;
         HashMap<String, String> t1;
-        HashMap<String, Double> credits = new HashMap<>();
-        HashMap<String, Double> grades = new HashMap<>();
+        HashMap<String, BigDecimal> credits;
+        HashMap<String, BigDecimal> grades;
         ArrayList<String> t2 = new ArrayList<>();
         
         try {
@@ -87,39 +89,34 @@ public class MP1 {
         		while((content=br1.readLine())!=null){
 //        			t1.clear();
         			t1 = new HashMap<>();
-//        			credits.clear();
-//        			grades.clear();
+        			credits = new HashMap<>();
+        			grades = new HashMap<>();
         			if (content!=null) {
     					t1.put(content.substring(0, 8), content.substring(8));
+    					credits.put(content.substring(0, 8), new BigDecimal("0"));
+    					grades.put(content.substring(0, 8), new BigDecimal("0"));
     					numberOfIO++;
         			}
         			for (int i = 1;i<bbs1;i++) {
         				if ((content=br1.readLine())!=null) {
         					t1.put(content.substring(0, 8), content.substring(8));
-        					numberOfIO++;
-        					credits.put(content.substring(0, 8), (double) 0);
-        					grades.put(content.substring(0, 8), (double) 0);
+        					credits.put(content.substring(0, 8), new BigDecimal("0"));
+        					grades.put(content.substring(0, 8), new BigDecimal("0"));
         				}
-					
+        				
         			}
+//        			System.out.println(t1.size());
+//    				System.out.println(credits.size());
+//    				System.out.println(grades.size());
 //        			System.out.println(t1.size());
         			br2 = new BufferedReader(new FileReader(inFile2));
         			while((content=br2.readLine())!=null){
         				if (t1.get(content.substring(0, 8))!=null) {
-    						bw1.write(t1.get(content.substring(0, 8))+content);
+    						bw1.write(content.substring(0, 8)+t1.get(content.substring(0, 8))+content.substring(8));
     						bw1.newLine();
-    						if (credits.get(content.substring(0, 8))!=null) {
-								credits.put(content.substring(0, 8), credits.get(content.substring(0, 8)) + Double.parseDouble(content.substring(21, 23)));
-							} else{
-								credits.put(content.substring(0, 8),Double.parseDouble(content.substring(21, 23)));
-							}
-							if (grades.get(content.substring(0, 8))!=null) {
-								//grades.put(content.substring(0, 8), grades.get(content.substring(0, 8)) + Double.parseDouble(content.substring(21, 23)));}
-								grades.put(content.substring(0, 8), grades.get(content.substring(0, 8)) + convertGrade(content.substring(23)) * Double.parseDouble(content.substring(21, 23)));
-							}
-    						else{
-								grades.put(content.substring(0, 8),Double.parseDouble(content.substring(21, 23)));
-							}
+    						credits.put(content.substring(0, 8), credits.get(content.substring(0, 8)).add(new BigDecimal(content.substring(21, 23).trim())));
+    						grades.put(content.substring(0, 8),grades.get(content.substring(0, 8)).add(new BigDecimal(content.substring(21, 23).trim()).multiply(new BigDecimal(convertGrade(content.substring(23)))))); 
+    						
         				}
 	        			
         			}
@@ -129,7 +126,7 @@ public class MP1 {
 
 					for(String key: t1.keySet()){
 						if(grades.get(key)!=null && credits.get(key)!=null) {
-							bw2.write(key + " " + (double) Math.round((grades.get(key) / credits.get(key))*10)/10+"\n");
+							bw2.write(key + " " + grades.get(key).divide(credits.get(key), RoundingMode.HALF_UP)+"\n");
 						}
 					}
         		}
